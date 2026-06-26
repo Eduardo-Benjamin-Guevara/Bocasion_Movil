d0629becd89e7a072c5d29cf48795dcd0d6a65a5
Eduardo-Benjamin-Guevara <N00331031@upn.pe>
2026-06-25 22:09:28 -0500

    Implementacion de funcion de actualizar contraseña

--- app/src/main/java/com/proyecto/capstone/activities/general/ProfileFragment.java
+++ app/src/main/java/com/proyecto/capstone/activities/general/ProfileFragment.java
@@ -137,6 +137,29 @@ public class ProfileFragment extends Fragment {
                         Toast.makeText(getContext(), "Error al actualizar nombre: " + e.getMessage(), Toast.LENGTH_SHORT).show()
                 );
     }
+        
+    private void updatePassword(String currentPass, String newPass) {
+        AuthCredential credential = EmailAuthProvider.getCredential(firebaseUser.getEmail(), currentPass);
+
+        firebaseUser.reauthenticate(credential)
+                .addOnSuccessListener(aVoid -> {
+
+                    firebaseUser.updatePassword(newPass)
+                            .addOnSuccessListener(aVoid2 -> {
+                                Toast.makeText(getContext(), "Contraseña actualizada exitosamente.", Toast.LENGTH_SHORT).show();
+                                currentPassEdit.setText("");
+                                newPassEdit.setText("");
+                            })
+                            .addOnFailureListener(e ->
+                                    Toast.makeText(getContext(), "Error al actualizar contraseña: " + e.getMessage(), Toast.LENGTH_LONG).show()
+                            );
+
+                })
+                .addOnFailureListener(e ->
+                        Toast.makeText(getContext(), "La contraseña actual es incorrecta.", Toast.LENGTH_LONG).show()
+                );
+    }
+
 
 
     private void performLogout() {
